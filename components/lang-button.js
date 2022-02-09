1f7917d1538b9bdc90ecb6b92b297deb741fe7bc
YPariset <yoann.pariset@edu.itescia.fr>
2022-02-09 22:54:59 +0100

    animation button navbar fix

--- components/lang-button.js
+++ components/lang-button.js
@@ -1,6 +1,5 @@
-import { AnimatePresence, motion } from 'framer-motion'
-import { useColorModeValue, Button, Divider, Stack } from '@chakra-ui/react';
-import ReactCountryFlag from "react-country-flag"
+import { Button, Divider, Stack } from '@chakra-ui/react'
+import ReactCountryFlag from 'react-country-flag'
 import { useRouter } from 'next/router'
 import NextLink from 'next/link'
 
@@ -11,34 +10,31 @@ const LangButton = () => {
 
   return (
     <div style={{ display: 'flex' }}>
-      <AnimatePresence exitBeforeEnter initial={false}>
-      <motion.div
-        style={{ display: 'flex' }}
-        key={useColorModeValue('light', 'dark')}
-        initial={{ y: -20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        exit={{ y: 20, opacity: 0 }}
-        transition={{ duration: 0.2 }}
-      >
-        <NextLink
+      <NextLink
         activeClassName={locale === 'en-US'}
         href={linkUS}
         passHref
         locale="en-US"
       >
-        <Button aria-label="Toggle lang" colorScheme="transparent" marginRight={0.5}>
-        <ReactCountryFlag
-                countryCode="US"
-                svg
-                style={{
-                    width: '1em',
-                    height: '1em',
-                }}
-                title="US"
-            />
+        <Button
+          aria-label="Toggle lang"
+          colorScheme="transparent"
+          marginRight={0.5}
+        >
+          <ReactCountryFlag
+            countryCode="US"
+            svg
+            style={{
+              width: '1em',
+              height: '1em'
+            }}
+            title="US"
+          />
         </Button>
       </NextLink>
+
       <Divider orientation="vertical" />
+
       <NextLink
         activeClassName={locale === 'fr-FR'}
         href={linkFR}
@@ -46,22 +42,24 @@ const LangButton = () => {
         locale="fr-FR"
       >
         <Stack direction="row" spacing={4}>
-          <Button  aria-label="Toggle lang" colorScheme="transparent" marginRight={5} marginLeft={0.5}>
-          <ReactCountryFlag
-                countryCode="FR"
-                svg
-                style={{
-                  width: '1em',
-                  height: '1em',
-                }}
-                title="FR"
+          <Button
+            aria-label="Toggle lang"
+            colorScheme="transparent"
+            marginRight={5}
+            marginLeft={0.5}
+          >
+            <ReactCountryFlag
+              countryCode="FR"
+              svg
+              style={{
+                width: '1em',
+                height: '1em'
+              }}
+              title="FR"
             />
           </Button>
         </Stack>
       </NextLink>
-     
-      </motion.div>
-    </AnimatePresence>
     </div>
   )
 }
